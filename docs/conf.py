68c4fdd4a1e9d84187ff0854f4b9b11e8a213fad
Rami Ariss <ramiariss@gmail.com>
2021-09-14 04:49:18 -0400

    Adding Sphinx documentation capabilities and Github action to auto-build documentation

---
+++ docs/conf.py
@@ -0,0 +1,73 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# This file only contains a selection of the most common options. For a full
+# list see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Path setup --------------------------------------------------------------
+
+# If extensions (or modules to document with autodoc) are in another directory,
+# add these directories to sys.path here. If the directory is relative to the
+# documentation root, use os.path.abspath to make it absolute, like shown here.
+#
+import os
+import sys
+_HERE = os.path.dirname(__file__)
+_ROOT_DIR = os.path.abspath(os.path.join(_HERE, '..'))
+_PACKAGE_DIR = os.path.abspath(os.path.join(_HERE, '../evrptwv2g'))
+# _SUBPACKAGE_DIR1 = os.path.abspath(os.path.join(_HERE, '../load_profitability/methods'))
+# _SUBPACKAGE_DIR2 = os.path.abspath(os.path.join(_HERE, '../load_profitability/utils'))
+
+sys.path.insert(0, _ROOT_DIR)
+sys.path.insert(0, _PACKAGE_DIR)
+# sys.path.insert(0, _SUBPACKAGE_DIR1)
+# sys.path.insert(0, _SUBPACKAGE_DIR2)
+
+# import load_profitability
+
+
+# -- Project information -----------------------------------------------------
+
+project = 'EVRP-TW-V2G'
+copyright = '2021, Rami Ariss'
+author = 'Rami Ariss'
+
+# The full version, including alpha/beta/rc tags
+release = '1.0'
+
+
+# -- General configuration ---------------------------------------------------
+
+# Add any Sphinx extension module names here, as strings. They can be
+# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
+# ones.
+extensions = [
+    'sphinx_rtd_theme',
+    'sphinx.ext.autodoc',
+    'sphinx.ext.autosummary'
+]
+
+autosummary_generate = True
+
+autosummary_mock_imports = []
+
+# Add any paths that contain templates here, relative to this directory.
+templates_path = ['_templates']
+
+# List of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files.
+# This pattern also affects html_static_path and html_extra_path.
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+
+# -- Options for HTML output -------------------------------------------------
+
+# The theme to use for HTML and HTML Help pages.  See the documentation for
+# a list of builtin themes.
+#
+html_theme = 'sphinx_rtd_theme'
+
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
+html_static_path = ['_static']
---
+++ docs/index.rst
@@ -0,0 +1,26 @@
+.. Load Profitability documentation master file, created by
+   sphinx-quickstart on Mon Aug 23 17:12:57 2021.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to EVRP-TW-V2G's documentation!
+==============================================
+
+We present a novel extension of the Electric Vehicle Routing Problem (E-VRP) that co-optimizes fleet
+sizing, vehicle routing, and vehicle-to-grid (V2G) charging and discharging decisions for
+time-varying energy arbitrage and peak shaving to maximize net amortized profit.
+
+.. autosummary::
+   :toctree: _autosummary
+   :template: custom-module-template.rst
+   :recursive:
+
+   evrptwv2g
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
